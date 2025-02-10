d578f78c0c99634332823bea486925a0f4ed8c4e
Fr0zen3k <jan.kresic@circuitmess.com>
2025-02-10 13:01:13 +0100

    Comm misc

--- main/src/Services/Comm.cpp
+++ main/src/Services/Comm.cpp
@@ -6,7 +6,7 @@
 void Comm::tick(float deltaTime) noexcept {
     Super::tick(deltaTime);
 
-
+    // TODO read incoming data here  (if needed... if not, remove this and set Comm to be object and not async entity)
 }
 
 void Comm::sendDriveDir(float dir) noexcept {
@@ -43,7 +43,7 @@ void Comm::sendPacket(Object *object) noexcept {
     std::vector<uint8_t> data;
     byteArrayFromObject(object, data);
 
-    std::vector<uint8_t> sizeData;
+    std::vector<uint8_t> sizeData(sizeof(size_t));
     size_t size = data.size();
 
     memcpy(sizeData.data(), &size, sizeof(size_t));
